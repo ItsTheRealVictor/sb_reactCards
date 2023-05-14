d5d018acf9083f0a914bccc84a6d022be2b2208b
Victor Delgado <valexdelgado@gmail.com>
2023-05-13 19:33:15 -0700

    react cards (finished weeks ago  but I forgot to init a repo and commit)

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Deck from './Deck'
+
+function App() {
+
+  return (
+    <>
+    <Deck/>
+    </>
+  )
+}
+
+export default App
